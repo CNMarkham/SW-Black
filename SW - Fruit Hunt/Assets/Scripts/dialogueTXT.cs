ec99f586137fde7ca60164f01f643e9c89490ac2
no <markhamonca@codeninjas.com>
2025-02-22 11:56:12 -0500

    cdf
    finished dialogueBox appearing, and planning for the code storing system.

--- SW - Fruit Hunt/Assets/Scripts/dialogueTXT.cs
+++ SW - Fruit Hunt/Assets/Scripts/dialogueTXT.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dialogueTXT : MonoBehaviour
+public class CNMDialogueTXT : MonoBehaviour
 {
+    public GameObject CNMBox;
+    public GameObject HelgaBox;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,37 @@ public class dialogueTXT : MonoBehaviour
         //if we're colliding (onTrigger) with C.N.M's collider
         if(collision.gameObject.tag == "Player" && gameObject.tag == "C.N.M")
         {
+            //making CNM's dialogue box appear when we collide with the trigger
+            CNMBox.SetActive(true);
+        }
+        //else
+        //{
+            //making CNM's dialogue box disappear when we don't collide the trigger
+            //CNMBox.SetActive(false);
+        //}
 
+        //if we're colliding (onTrigger) with Helga's collider
+        if(collision.gameObject.tag == "Player" && gameObject.tag == "Helga")
+        {
+            //making Helga's dialogue box appear when we collide with the trigger
+            HelgaBox.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        //if we leave the trigger
+        if (collision.gameObject.tag == "Player" && gameObject.tag == "C.N.M")
+        {
+            //making the dialogue box disappear
+            CNMBox.SetActive(false);
+        }
+
+        //if we leave the trigger
+        if (collision.gameObject.tag == "Player" && gameObject.tag == "Helga")
+        {
+            //making the dialogue box disappear
+            HelgaBox.SetActive(false);
         }
     }
 }
