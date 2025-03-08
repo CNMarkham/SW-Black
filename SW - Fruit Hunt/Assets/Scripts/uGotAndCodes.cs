6c3a567ffa95c29723269e5a7eec0851226fd99e
Do <markhamonca@codeninjas.com>
2025-03-08 11:54:18 -0500

    cdf
    IF UGOT && CODES SET.ACTIVE(TRUE) CODE 😠

---
+++ SW - Fruit Hunt/Assets/Scripts/uGotAndCodes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uGotAndCodes : MonoBehaviour
+{
+    public GameObject uGot;
+    public GameObject Codes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
